f393aba675535f57ddedfa65ddae81f8dfb00b39
wachtern <wachternlvg@gmail.com>
2025-03-26 18:38:56 +0100

    feat: add theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import { useState } from "react";
 import "./App.css";
 import List from "./views/List";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./providers/theme";
+import { useState } from "react";
 
 function App() {
   const [page, setPage] = useState<number>(0);
-
-  return <>{page === 0 && <List />}</>;
+  return <ThemeProvider theme={theme}>{page === 0 && <List />}</ThemeProvider>;
 }
 
 export default App;
